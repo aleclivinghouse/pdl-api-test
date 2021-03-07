eccccecaea95124b6ab502d28d6f88683a384af1
aleclivinghouse <aleclivinghouse@yahoo.com>
2021-03-06 16:06:44 -0800

    Create mock.js

---
+++ test/mock.js
@@ -0,0 +1,17 @@
+//put your data in these arrays
+const phoneNumbers = [];
+// const emails = [];
+const nameCompany = [];
+const linkedInUrls = [];
+const linkedInIds = [];
+
+const emails = [];
+
+
+module.exports = {
+    phoneNumbers,
+    emails,
+    nameCompany,
+    linkedInUrls,
+    linkedInIds
+};
